8cf5cfa2f87404a598c51866c1534b018b991ee3
Nikolai Shangin <shanginn@gmail.com>
2023-06-21 11:53:07 +0800

    Added StateMachine.lua, BaseState.lua, PlayState.lua, ScoreState.lua, TitleScreenState.lua, and CountdownState.lua files.

--- main.lua
+++ main.lua
@@ -5,6 +5,13 @@ require 'Bird'
 require 'Pipe'
 require 'PipePair'
 
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/PlayState'
+require 'states/ScoreState'
+require 'states/TitleScreenState'
+require 'states/CountdownState'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -36,11 +43,25 @@ function love.load()
 
     math.randomseed(os.time())
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = false,
     })
+
+    gStateMachine = StateMachine {
+        ['title'] = function () return TitleScreenState() end,
+        ['play'] = function () return PlayState() end,
+        ['score'] = function () return ScoreState() end,
+        ['countdown'] = function () return CountdownState() end,
+    }
+    gStateMachine:change('title')
 
     love.keyboard.keysPressed = {}
 end
@@ -62,42 +83,12 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    if scrolling then
-        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
-        spawnTimer = spawnTimer + dt
+    gStateMachine:update(dt)
 
-        if spawnTimer > 2 then
-            local y = math.max(-PIPE_HEIGHT + 10, 
-                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-
-            lastY = y
-
-            table.insert(pipePairs, PipePair(y))
-            spawnTimer = 0
-        end
-
-        bird:update(dt)
-
-        for k, pipePair in pairs(pipePairs) do
-            pipePair:update(dt)
-
-            for l, pipe in pairs(pipePair.pipes) do
-                if bird:collides(pipe) then
-                    scrolling = false
-                end
-            end
-        end
-
-        for k, pipePair in pairs(pipePairs) do
-            if pipePair.remove then
-                table.remove(pipePairs, k)
-            end
-        end
-
-        love.keyboard.keysPressed = {}
-    end
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
@@ -106,13 +97,9 @@ function love.draw()
     -- draw the background starting at top left (0, 0)
     love.graphics.draw(background, -backgroundScroll, 0)
 
-    for k, pipePair in pairs(pipePairs) do
-        pipePair:render()
-    end
+    gStateMachine:render()
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-
-    bird:render()
     
     push:finish()
 end
